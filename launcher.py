0d288aff54e41a0826bf2e24251c5b82978e85e7
clerie <cleriese@gmail.com>
2015-12-31 13:13:28 +0100

    Create launcher.py

---
+++ launcher.py
@@ -0,0 +1,20 @@
+import os
+
+print '=Display Komando='
+os.system('sudo python launcher/welcome.py')
+print 'Type "quit" to exit.'
+print ''
+
+quit = 0
+
+while quit == 0:
+	cmd = raw_input("> ")
+
+	if cmd == 'quit':
+		quit = 1
+	else:
+		print''
+		print '-'
+		os.system('sudo python plugins/' + cmd + '.py')
+		print '-'
+		print ''
